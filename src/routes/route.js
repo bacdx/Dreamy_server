49a90d8310f92840b02d1c9dbe12e1ebbfdf0467
ky <kydo648@gmail.com>
2023-11-07 08:45:30 +0700

    ph24886

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,10 @@ const express=require('express');
 const product=require('./product');
 const api=require('./api');
 const notification=require('./notification');
+const received=require('./received');
 
 function route(app){
  
-
 
 app.use('/login',weblogin);
 
@@ -14,6 +14,7 @@ app.use('/product',product);
 
 app.use('/api',api);
 
+app.use('/api',received);
 
 app.use('/notification',notification);
 
